a70f64807caa582f34ab66627e4aa9f95b59a201
Wazzups <pedromespadinha95@gmail.com>
2024-11-01 23:56:41 +0000

    Initialize Spring Project with PostgreSQL Configuration
    - Configure PostgreSQL for database persistence
    - Add entities for Role and ApplicationUser to handle user and role data

---
+++ src/main/java/com/wazzups/ftwitterbackend/FtwitterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wazzups.ftwitterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtwitterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FtwitterBackendApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/wazzups/ftwitterbackend/models/Role.java
@@ -0,0 +1,52 @@
+package com.wazzups.ftwitterbackend.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private Long roleId;
+
+    private String authority;
+
+    public Role() {
+    }
+
+    public Role(Long roleId, String authority) {
+        this.roleId = roleId;
+        this.authority = authority;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+            "roleId=" + roleId +
+            ", authority='" + authority + '\'' +
+            '}';
+    }
+}
